52a1d3c4c9e09db5328535157740999de36679f9
devlee77 <tjdcks0928@naver.com>
2020-11-06 23:40:17 +0900

    add LambdaTest

---
+++ kotlin_part1/src/shap04/section1/LambdaTest.kt
@@ -0,0 +1,17 @@
+package shap04.section1
+
+fun main() {
+    val result: Int
+
+    //val multi = {a: Int, b:Int -> a * b}
+    val multi: (a: Int, b: Int) -> Int = {a, b ->
+        println("a:$a, b:$b")
+        a * b
+    }
+
+    val nestedLambda: () -> () -> Unit = { { println("nestedLambda") } }
+
+    result = multi(10, 20)
+    println(result)
+
+}
