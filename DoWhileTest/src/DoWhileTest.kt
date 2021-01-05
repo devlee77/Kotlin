0964e491af8ce87acb5de63d65b911190198ce74
devlee77 <tjdcks0928@naver.com>
2021-01-05 18:06:11 +0900

    DowhileTest complete

---
+++ DoWhileTest/src/DoWhileTest.kt
@@ -0,0 +1,14 @@
+fun main() {
+    //5 -> 12345, 23451, 34512..
+
+    do {
+        print("Enter the number : ")
+        val input = readLine()!!.toInt()
+        for (i in 0 until input) {
+            for (j in 0 until input) {
+                print((i + j) % input + 1)
+            }
+            println()
+        }
+    } while (input != 0)
+}
