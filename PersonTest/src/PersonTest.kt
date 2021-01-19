d13e63e30a2aaee9734f0711061cd7e2fb87ce66
devlee77 <tjdcks0928@naver.com>
2021-01-19 23:50:38 +0900

    PersonTest complete

---
+++ PersonTest/src/PersonTest.kt
@@ -0,0 +1,11 @@
+ class Person(_id: Int, _name: String, _age:Int){
+     var id: Int = _id
+     var name: String = _name
+     var age: Int = _age
+ }
+
+ fun main() {
+     var person = Person(1,"Kildong", 30)
+
+     
+ }
