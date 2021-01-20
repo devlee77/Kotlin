29d7a8382af807bbd40f9943193412b50e78060c
devlee77 <tjdcks0928@naver.com>
2021-01-20 23:15:08 +0900

    LateinitTest complete

---
+++ LateinitTest/src/LateinitTest.kt
@@ -0,0 +1,20 @@
+class Person {
+
+    lateinit var name: String
+
+    fun test(){
+        if (!::name.isInitialized) {
+            println("not Initialized")
+        } else {
+            println("Initialized")
+        }
+    }
+}
+
+fun main() {
+    val person = Person()
+
+    person.test()
+    person.name = "Kildong"
+    person.test()
+}
