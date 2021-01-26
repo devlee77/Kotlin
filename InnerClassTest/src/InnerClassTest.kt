4cb8abe47c0df1bc8de4079478584788c92819cf
devlee77 <tjdcks0928@naver.com>
2021-01-26 20:36:32 +0900

    InnerClass complete

---
+++ InnerClassTest/src/InnerClassTest.kt
@@ -0,0 +1,34 @@
+interface Switcher {
+    fun on(): String
+}
+
+class Smartphone(val model: String) {
+
+    private val cpu = "Exynos"
+
+    inner class ExternalStorage(val size: Int) {
+        fun getInfo() = println("$model : Installed on $cpu with $size  Gb")
+    }
+
+    fun powerOn(): String {
+        class Led(val color: String){
+            fun blink() = "Blinking $color on $model"
+        }
+        val powerStatus = Led("Red")
+        //return powerStatus.blink()
+
+        val powerSwitch = object : Switcher{
+            override fun on(): String {
+                return powerStatus.blink()
+            }
+        }
+        return powerSwitch.on()
+    }
+}
+
+fun main() {
+
+    val mySdcard = Smartphone("s7").ExternalStorage(128)
+    mySdcard.getInfo()
+    println(Smartphone("s8").powerOn())
+}
